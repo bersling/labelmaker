31e4ac667cacada7475a1cbf2816e053297ccfda
daniel <daniel@taskbase.com>
2022-01-05 21:03:55 +0100

    v1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,17 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+
+/*
+
+0: Vornamen
+1: Nachnamen
+2: Vorname Partner
+3: Nachname Partner
+4: Strasse
+5: PLZ
+6: Stadt
+7: Land
+
+* */
 
 @Component({
   selector: 'app-root',
@@ -7,4 +20,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'labelmaker';
+  numVerticalLabels: number = 0;
+  numHorizontalLabels: number = 0;
+
+  getWidth(): string {
+    return (1/this.numHorizontalLabels)*100 + 'vw';
+  }
+
+  labelsStr: string = "";
+  get labels(): string[][] {
+    return this.labelsStr.split(`\n`).map(line => line.split(`\t`));
+  }
 }
